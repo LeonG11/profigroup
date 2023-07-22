d103fdbdd03748b782ebaab1c3a799f9a3e301cf
LeonG11 <kkacanov198@gmail.com>
2023-07-22 15:18:43 +0300

    First init

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='Header'>
+      <a href="#">Admin</a>
+      <ul className="HeaderList">
+        <li className="Headerbutton"><a href="#"></a>Заказы</li>
+        <li className="Headerbutton">Производство</li>
+        <li className="Headerbutton">Меню</li>
+      </ul>
+
+
+
+    </div>
+  )
+}
